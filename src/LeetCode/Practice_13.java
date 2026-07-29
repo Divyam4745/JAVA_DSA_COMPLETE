8306b1c354de673629a23254315b845d20be6bf7
Divyam4745 <divyamvishwakarma34@gmail.com>
2026-07-29 23:23:47 +0530

    LeetCode Problem

---
+++ src/LeetCode/Practice_13.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Practice_13 {
+    static List<Integer> practic(int arr[][]) {
+        List<Integer> result = new ArrayList<>();
+        int n = arr.length;
+        int m = arr[0].length;
+        for (int row = 0; row < arr.length; row++) {
+            int sum = 0;
+            for (int col = 0; col < arr.length; col++) {
+                int value = arr[row][col];
+                sum = sum + value;
+            }
+            result.add(sum);
+        }
+        return result;
+    }
+        public static void main(String[] args) {
+            int[][] arr = {
+                    {1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9}
+            };
+            List<Integer> ans = practic(arr);
+            System.out.println(ans);
+
+    }
+}
