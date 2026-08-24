21b4d925fa9353d0433f030adccb4aa64a952983
Divyam4745 <divyamvishwakarma34@gmail.com>
2026-08-24 23:20:49 +0530

    Factroical of number

---
+++ src/Recursion/Leetcode_1.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class Leetcode_1 {
+    static long factroial(int n){
+        //base case
+    if (n==0){
+        return 1;
+    }
+
+    long ans = n* factroial(n-1);
+    return  ans;
+    }
+    static void main() {
+        int n=5;
+        System.out.println("The factroical is : " + factroial(n));
+
+    }
+}
