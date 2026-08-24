7efd2dea76d020d61f303dfe15e2db9bdfc74244
Divyam4745 <divyamvishwakarma34@gmail.com>
2026-08-25 00:49:24 +0530

    climbing of stairs

---
+++ src/Recursion/Leetcode_4.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class Leetcode_4 {
+    static int climbing(int n)  {
+        if (n==1){
+            return 1;
+        }
+        if (n==2){
+            return 2;
+        }
+        int ans = climbing(n-1) + climbing(n-2);
+        return ans;
+    }
+
+    static void main(String[] args) {
+        System.out.println(climbing(5));
+    }
+}
