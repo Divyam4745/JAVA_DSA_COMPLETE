72eae78125d1da39c4572fc5de5f01f124a24900
Divyam Vishwakarma <divyamvishwakarma34@gmail.com>
2026-09-10 23:07:44 +0530

    Time: 4 ms (99.66%), Space: 52 MB (98.79%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        int size = 1 << n;
+        List<Integer> ans = new ArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+            ans.add(i ^ (i >> 1));
+        }
+
+        return ans;
+    }
+}
