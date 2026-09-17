26e18611c81ab6075ff250c4ab54be95fa902ddc
Divyam Vishwakarma <divyamvishwakarma34@gmail.com>
2026-09-17 23:18:25 +0530

    Time: 1 ms (99.99%), Space: 43.6 MB (47.96%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,60 +1,36 @@
 class Solution {
+    // 0 = uncalculated, 1 = true, -1 = false
+    byte[][] memo;
+
     public boolean isMatch(String s, String p) {
-        int m = s.length();
-        int n = p.length();
+        // Use primitive byte array for raw speed and minimal memory
+        memo = new byte[s.length() + 1][p.length() + 1];
+        return dfs(0, 0, s.toCharArray(), p.toCharArray());
+    }
+
+    private boolean dfs(int i, int j, char[] s, char[] p) {
+        // Return precalculated result immediately
+        if (memo[i][j] != 0) {
+            return memo[i][j] == 1;
+        }
+
+        boolean ans;
         
-        // Convert to char arrays for faster access
-        char[] sArr = s.toCharArray();
-        char[] pArr = p.toCharArray();
+        if (j == p.length) {
+            ans = (i == s.length);
+        } else {
+            boolean firstMatch = (i < s.length && (p[j] == s[i] || p[j] == '.'));
 
-        // Only store the previous row and the current row
-        boolean[] prev = new boolean[n + 1];
-        boolean[] curr = new boolean[n + 1];
-
-        // Empty string matches empty pattern
-        prev[0] = true;
-
-        // Base case: patterns like a*, a*b*, a*b*c* matching an empty string
-        for (int j = 2; j <= n; j++) {
-            if (pArr[j - 1] == '*') {
-                prev[j] = prev[j - 2];
+            if (j + 1 < p.length && p[j + 1] == '*') {
+                // Short-circuit evaluation: if dfs(i, j+2) is true, the second half is skipped entirely
+                ans = (dfs(i, j + 2, s, p) || (firstMatch && dfs(i + 1, j, s, p)));
+            } else {
+                ans = firstMatch && dfs(i + 1, j + 1, s, p);
             }
         }
 
-        for (int i = 1; i <= m; i++) {
-            // A non-empty string will never match an empty pattern
-            curr[0] = false; 
-            char sc = sArr[i - 1];
-
-            for (int j = 1; j <= n; j++) {
-                char pc = pArr[j - 1];
-
-                if (pc == '.' || pc == sc) {
-                    curr[j] = prev[j - 1];
-                } 
-                else if (pc == '*') {
-                    // '*' matches zero occurrences
-                    curr[j] = curr[j - 2];
-                    
-                    char prevChar = pArr[j - 2];
-                    // '*' matches one or more occurrences
-                    if (prevChar == '.' || prevChar == sc) {
-                        curr[j] = curr[j] || prev[j];
-                    }
-                } 
-                else {
-                    // Mismatched characters
-                    curr[j] = false; 
-                }
-            }
-            
-            // Swap prev and curr for the next iteration (reusing memory)
-            boolean[] temp = prev;
-            prev = curr;
-            curr = temp;
-        }
-
-        // Because we swap at the end of the loop, the final answer rests in 'prev'
-        return prev[n];
+        // Save result as 1 (true) or -1 (false)
+        memo[i][j] = (byte) (ans ? 1 : -1);
+        return ans;
     }
 }
