10ed0565d6bca97781995523f84dc2fe0e79ea36
Divyam Vishwakarma <divyamvishwakarma34@gmail.com>
2026-09-18 23:23:03 +0530

    Time: 1 ms (99.99%), Space: 43.3 MB (77.02%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,36 +1,51 @@
 class Solution {
-    // 0 = uncalculated, 1 = true, -1 = false
-    byte[][] memo;
-
     public boolean isMatch(String s, String p) {
-        // Use primitive byte array for raw speed and minimal memory
-        memo = new byte[s.length() + 1][p.length() + 1];
-        return dfs(0, 0, s.toCharArray(), p.toCharArray());
-    }
-
-    private boolean dfs(int i, int j, char[] s, char[] p) {
-        // Return precalculated result immediately
-        if (memo[i][j] != 0) {
-            return memo[i][j] == 1;
-        }
-
-        boolean ans;
+        char[] sc = s.toCharArray();
+        char[] pc = p.toCharArray();
+        int m = sc.length;
+        int n = pc.length;
         
-        if (j == p.length) {
-            ans = (i == s.length);
-        } else {
-            boolean firstMatch = (i < s.length && (p[j] == s[i] || p[j] == '.'));
-
-            if (j + 1 < p.length && p[j + 1] == '*') {
-                // Short-circuit evaluation: if dfs(i, j+2) is true, the second half is skipped entirely
-                ans = (dfs(i, j + 2, s, p) || (firstMatch && dfs(i + 1, j, s, p)));
-            } else {
-                ans = firstMatch && dfs(i + 1, j + 1, s, p);
+        // We only need a 1D array of size n + 1 to store the previous row's state
+        boolean[] dp = new boolean[n + 1];
+        
+        // Base case setting for i = m (when the string s is exhausted)
+        dp[n] = true; 
+        for (int j = n - 1; j >= 0; j--) {
+            // If we have a '*', we can skip the character and the '*' itself
+            if (j + 1 < n && pc[j + 1] == '*') {
+                dp[j] = dp[j + 2];
             }
         }
-
-        // Save result as 1 (true) or -1 (false)
-        memo[i][j] = (byte) (ans ? 1 : -1);
-        return ans;
+        
+        // Bottom-up evaluation (from i = m - 1 down to 0)
+        for (int i = m - 1; i >= 0; i--) {
+            // 'next' represents dp[i+1][j+1] (the diagonal value) from the old row
+            boolean next = dp[n]; 
+            
+            // An empty pattern can never match a non-empty remaining string
+            dp[n] = false; 
+            
+            for (int j = n - 1; j >= 0; j--) {
+                // 'temp' caches the current dp[j] (which is effectively dp[i+1][j]) 
+                // before we overwrite it for the current row.
+                boolean temp = dp[j]; 
+                
+                boolean firstMatch = (pc[j] == sc[i] || pc[j] == '.');
+                
+                if (j + 1 < n && pc[j + 1] == '*') {
+                    // dp[j + 2] comes from the current row (already calculated in this loop)
+                    // temp comes from the previous row (i+1)
+                    dp[j] = dp[j + 2] || (firstMatch && temp);
+                } else {
+                    // 'next' comes from the diagonal (i+1, j+1)
+                    dp[j] = firstMatch && next;
+                }
+                
+                // Advance 'next' to act as the diagonal for the next column calculation
+                next = temp;
+            }
+        }
+        
+        return dp[0];
     }
 }
