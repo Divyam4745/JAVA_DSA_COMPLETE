d4bb37aade63b9ba3c421a149a8454633e36060c
Divyam Vishwakarma <divyamvishwakarma34@gmail.com>
2026-09-19 23:39:35 +0530

    Time: 0 ms (100%), Space: 43.1 MB (64.09%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,32 +1,32 @@
 class Solution {
     public int numDecodings(String s) {
-
-        int n = s.length();
-
-        if (n == 0 || s.charAt(0) == '0') {
+        // Base checks
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
             return 0;
         }
 
-        // prev2 = dp[i-2]
-        // prev1 = dp[i-1]
         int prev2 = 1;
         int prev1 = 1;
+        int n = s.length();
 
         for (int i = 1; i < n; i++) {
-
             int current = 0;
+            char currChar = s.charAt(i);
+            char prevChar = s.charAt(i - 1);
 
-            // One digit
-            if (s.charAt(i) != '0') {
-                current += prev1;
+            // 1. One digit decoding
+            if (currChar != '0') {
+                current = prev1; 
             }
 
-            // Two digits
-            int two = (s.charAt(i - 1) - '0') * 10
-                    + (s.charAt(i) - '0');
-
-            if (two >= 10 && two <= 26) {
+            // 2. Two digit decoding (checking characters directly)
+            if (prevChar == '1' || (prevChar == '2' && currChar <= '6')) {
                 current += prev2;
+            }
+
+            // 3. Early exit for invalid sequences like "30", "40", "00"
+            if (current == 0) {
+                return 0;
             }
 
             prev2 = prev1;
